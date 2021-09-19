76466db62815ccdac10addeb905eefca62753014
Echidna <AT130938@actvn.edu.vn>
2021-09-19 14:08:19 +0700

    remove merge header

--- FlaskApp/schedule.py
+++ FlaskApp/schedule.py
@@ -18,16 +18,11 @@ def create(domain, email, hours=None, minutes=None):
         print("This domain is avaliable!")
     else:
         job = my_cron.new(command=command, comment=comment)
-<<<<<<< HEAD
         if hours is not None:
             job.hour.every(hours)
         if minutes is not None:
             job.minute.every(minutes)
         my_cron.write(user='root')
-=======
-        job.minute.every(time)
-        my_cron.write(user=environ.get("USER"))
->>>>>>> e3b46280aed34928603577d7a12b04d5c502a226
         print(job.is_valid())
 
 
@@ -40,16 +35,11 @@ def edit(domain, email, hours=None, minutes=None):
             check = 1
             my_cron.remove(job)
             job = my_cron.new(command=command, comment=comment)
-<<<<<<< HEAD
             if hours is not None:
                 job.hour.every(hours)
             if minutes is not None:
                 job.minute.every(minutes)
             my_cron.write(user='root')
-=======
-            job.minute.every(time)
-            my_cron.write(user=environ.get("USER"))
->>>>>>> e3b46280aed34928603577d7a12b04d5c502a226
             print("Sucessfull!")
     if check == 0:
         print("Domain not found!")
@@ -62,11 +52,7 @@ def delete(domain):
         if job.comment == comment:
             check = 1
             my_cron.remove(job)
-<<<<<<< HEAD
             my_cron.write(user='root')
-=======
-            my_cron.write(user=environ.get("USER"))
->>>>>>> e3b46280aed34928603577d7a12b04d5c502a226
             print("Sucessfull!")
     if check == 0:
         print("Domain not found!")
--- screenshot.py
+++ screenshot.py
@@ -8,10 +8,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-<<<<<<< HEAD
-=======
-
->>>>>>> e3b46280aed34928603577d7a12b04d5c502a226
 if not os.path.exists("/opt/In0ri/FlaskApp/static/images/"):
     os.makedirs("/opt/In0ri/FlaskApp/static/images/")
 
@@ -25,34 +21,18 @@ def screenshot(url):
     options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(options=options)
 
-<<<<<<< HEAD
-=======
-
->>>>>>> e3b46280aed34928603577d7a12b04d5c502a226
     name = hashlib.md5(url.encode())
     try:
         driver.get(url)
         print("Screenshoting..." + url)
         time.sleep(6)
-<<<<<<< HEAD
         driver.get_screenshot_as_file(
             "/opt/In0ri/FlaskApp/static/images/" + name.hexdigest() + ".png"
         )
-=======
         driver.get_screenshot_as_file("/opt/In0ri/FlaskApp/static/images/" + name.hexdigest() + ".png")
->>>>>>> e3b46280aed34928603577d7a12b04d5c502a226
         driver.quit()
     except RuntimeError:
         print("URL " + url + " was died!")
         pass
-<<<<<<< HEAD
-=======
-    # resize gov_images
-    # print("resizing...")
-    # image = Image.open("/opt/In0ri/images/web_screenshot.png")
-    # # new_image = image.resize((250, 250))
-    # name = hashlib.md5(url.encode())
-    # image.save("/opt/In0ri/images/" + name.hexdigest() + ".png")
->>>>>>> e3b46280aed34928603577d7a12b04d5c502a226
 
     return "/opt/In0ri/FlaskApp/static/images/" + name.hexdigest() + ".png"
